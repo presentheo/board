b7ae891a15348309b543d92ed71b9160478c958e
presentheo <dbcjdgk@gmail.com>
2018-03-31 13:51:09 +0900

    js 수정
    1) 주석 추가
    2) 스토리지에서 seq번호 대조해서 뿌려주는 로직 수정

--- res/js/main.js
+++ res/js/main.js
@@ -15,7 +15,7 @@ function getNow(){
         }else{
             return m;
         }
-    };
+    }
     var month = getFullMonth(now.getMonth()+1);
     var date = now.getDate();
     var hour = now.getHours();
@@ -27,7 +27,7 @@ function getNow(){
 // 배열 선언
 var itemsArr = [];
 
-
+// 스토리지에서 마지막 시퀀스 번호 따기
 function getLastSeqNum(){
     var storedItems = JSON.parse(localStorage.getItem('items'));
     if (storedItems == null){
@@ -46,11 +46,11 @@ function addNewCol(){
         alert('내용을 입력해주세요!');
     }else{
         var item = {
-            seq: getLastSeqNum()+1,
+            seq: getLastSeqNum()+1, // 마지막 시퀀스 번호 +1 부여
             title: titleVal,
             content: contentVal,
             date: getNow()
-        }
+        };
         itemsArr.push(item);
     
         console.log(itemsArr);
@@ -59,37 +59,39 @@ function addNewCol(){
     }
 }
 
+// 저장 버튼에 이벤트 바인딩
 saveBtn.click(addNewCol);
 
-
+// 스토리지에 배열 저장하고 렌더링
 function saveItem(){
     localStorage.setItem('items', JSON.stringify(itemsArr));
     renderItem();
 }
 
+// DOM엘리먼트 템플릿 등록
 function getTemplate(url, seq, title, date){
-    return '<li class="column-item"><a class="column-item-anchor" href="'+url+'"><div class="column-item-num"><span>'+seq+'</span></div><div class="column-item-title"><span>'+title+'</span></div><div class="column-item-date"><span>'+date+'</span></div></a></li>';
+    return '<li class="column-item"><a class="column-item-anchor" href="'+url+'" target="_blank"><div class="column-item-num"><span>'+seq+'</span></div><div class="column-item-title"><span>'+title+'</span></div><div class="column-item-date"><span>'+date+'</span></div></a></li>';
 }
 
+// 렌더링
 function renderItem(){
     colList.html('');
     var storedItems = JSON.parse(localStorage.getItem('items'));
     if (storedItems !== null){
         itemsArr = storedItems;
         storedItems.forEach(function(e){
-            colList.append(getTemplate(getUrl(e.seq), e.seq, e.title, e.date));
-        })
+            colList.append(getTemplate(replaceUrl(e.seq), e.seq, e.title, e.date));
+        });
     }
 }
 
-
-function getUrl(n){
-    var url = window.location.href.replace('board.html', 'column.html');
-    return url+'?seq='+n;
+// url주소 치환
+function replaceUrl(n){
+    var url = window.location.href;
+    return (url.replace('board.html', 'column.html'))+'?seq='+n;
 }
 
 // 스토리지에 있는거 뿌려주는 로직
-
 var colTitle = $('#colTitle');
 var colContent = $('#colContent');
 
@@ -97,16 +99,18 @@ function loadCol(){
     var storedItems = JSON.parse(localStorage.getItem('items'));
     var url = window.location.href;
     var seqNum = url.split('?seq=')[1];
-    var item = storedItems[seqNum-1];
-
-    colTitle.html(item.title);
-    colContent.html(item.content);
+    for (var i=0; i<storedItems.length; i++){
+        if (storedItems[i].seq == seqNum){
+            colTitle.html(storedItems[i].title);
+            colContent.html(storedItems[i].content);
+        }
+    }
 }
 
 $(document).ready(function(){
     renderItem();
     loadCol();
-})
+});
 
 
 
